cb0b35beca00dcd9413ec50017800c1d41a7b7c6
fabiooportela <josannep15@gmail.com>
2023-01-11 10:02:20 -0300

    calculo

---
+++ scripts.js
@@ -0,0 +1,35 @@
+//document.getElementById("container").innerHTML = "conteudo";
+
+// let container =document.getElementById("container");
+
+// // let macas = 3;
+// // let banana = 3
+// // let soma = 2+4
+// // container.innerHTML = soma;
+
+
+// //container.innerHTML = "uma coisa";
+// //ontainer.append("outra a");
+
+// // let texto = "Meu Nome é " ;
+// // let nome = "fabio"
+// // let soma = texto + nome;
+// // container.innerHTML = soma;
+
+// const NASCIMENTO = "23/08/1997";
+// let nome = "fabio";
+// container.innerHTML = NASCIMENTO;
+
+// NASCIMENTO = "1997";
+
+// container.innerHTML = NASCIMENTO;
+
+function somar()
+{
+    let v1 = Number(valor1.value);
+    let v2 = Number(valor2.value);
+    document.getElementById("resultado").innerHTML = v1 + v2;
+}
+
+
+
